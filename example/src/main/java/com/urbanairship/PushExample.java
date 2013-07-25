c8a638ae36f8951720775a51b4f29dc1987aca70
Matt Hooge <mhooge@gmail.com>
2013-07-25 12:46:13 -0700

    Updated sample project in example directory
    - demonstrates how to bring in Urban Airship Library using Maven

---
+++ example/src/main/java/com/urbanairship/PushExample.java
@@ -0,0 +1,125 @@
+package com.urbanairship;
+
+import com.urbanairship.api.client.*;
+import com.urbanairship.api.push.model.Platform;
+import com.urbanairship.api.push.model.PlatformData;
+import com.urbanairship.api.push.model.PushPayload;
+import com.urbanairship.api.push.model.audience.Selectors;
+import com.urbanairship.api.push.model.notification.Notifications;
+import com.urbanairship.api.schedule.model.Schedule;
+import com.urbanairship.api.schedule.model.SchedulePayload;
+
+import org.apache.log4j.BasicConfigurator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+
+/*
+ * Copyright 2013 Urban Airship and Contributors
+ */
+public class PushExample {
+
+    private static final Logger logger = LoggerFactory.getLogger("com.urbanairship.api");
+
+    public void sendPush(){
+
+        String appKey = "appKey";
+        String appSecret = "appSecret";
+        // This is optional, one of many options for push.
+        String deviceToken = "deviceToken";
+
+
+        logger.debug(String.format("Make sure key and secret are set Key:%s Secret:%s",
+                                   appKey, appSecret));
+
+        APIClient apiClient = APIClient.newBuilder()
+                                       .setKey(appKey)
+                                       .setSecret(appSecret)
+                                       .build();
+        logger.debug(String.format("Setup an APIClient to handle the API call %s", apiClient.toString()));
+        logger.debug("Send the message");
+
+        PushPayload payload = PushPayload.newBuilder()
+                                         .setAudience(Selectors.deviceToken(deviceToken))
+                                         .setNotification(Notifications.notification("Urban Airship Push"))
+                                         .setPlatforms(PlatformData.of(Platform.IOS))
+                                         .build();
+
+        try {
+            APIClientResponse<APIPushResponse> response = apiClient.push(payload);
+            logger.debug("PUSH SUCCEEDED");
+            logger.debug(String.format("RESPONSE:%s", response.toString()));
+        }
+        catch (APIRequestException ex){
+            logger.error(String.format("APIRequestException " + ex));
+            logger.error("EXCEPTION " + ex.toString());
+
+            APIError apiError = ex.getError().get();
+            APIErrorDetails apiErrorDetails = apiError.getDetails().get();
+            logger.error("Error " + apiError.getError());
+            logger.error("Error details " + apiErrorDetails.getError());
+
+        }
+        catch (IOException e){
+            logger.error("IOException in API request " + e.getMessage());
+        }
+
+    }
+
+    public void sendScheduledPush(){
+        String appKey = "appKey";
+        String appSecret = "appSecret";
+
+        APIClient apiClient = APIClient.newBuilder()
+                                       .setKey(appKey)
+                                       .setSecret(appSecret)
+                                       .setBaseURI("https://go.urbanairship.com")
+                                       .setVersion(3)
+                                       .build();
+
+        PushPayload payload = PushPayload.newBuilder()
+                                         .setAudience(Selectors.all())
+                                         .setNotification(Notifications.alert("Scheduled API v3"))
+                                         .setPlatforms(PlatformData.of(Platform.IOS))
+                                         .build();
+
+        DateTime dt = DateTime.now().plusSeconds(60);
+        Schedule schedule = Schedule.newBuilder()
+                                    .setScheduledTimestamp(dt)
+                                    .build();
+
+        SchedulePayload schedulePayload = SchedulePayload.newBuilder()
+                                                         .setName("Urban Airship Scheduled Push")
+                                                         .setPushPayload(payload)
+                                                         .setSchedule(schedule)
+                                                         .build();
+
+        try {
+            APIClientResponse<APIScheduleResponse> response = apiClient.schedule(schedulePayload);
+            logger.debug("SCHEDULE PUSH SUCCEEDED");
+            logger.debug(String.format("RESPONSE:%s", response.toString()));
+        }
+        catch (APIRequestException ex){
+            logger.error(String.format("APIRequestException " + ex));
+            logger.error("EXCEPTION " + ex.toString());
+        }
+        catch (IOException e){
+            logger.error("Exception in API request -> " + e.getMessage());
+
+        }
+    }
+
+    public static void main(String args[]){
+        BasicConfigurator.configure();
+        logger.debug("Starting test push");
+        PushExample example = new PushExample();
+        example.sendPush();
+        example.sendScheduledPush();
+
+    }
+}
+
