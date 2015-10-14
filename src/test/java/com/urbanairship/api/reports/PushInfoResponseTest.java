e2082ce4245ce904dfc2bd506170607595e2cd16
Max Del Giudice <maxdelgiudice@gmail.com>
2015-10-14 12:52:30 -0700

    tests

---
+++ src/test/java/com/urbanairship/api/reports/PushInfoResponseTest.java
@@ -0,0 +1,70 @@
+package com.urbanairship.api.reports;
+
+import com.urbanairship.api.reports.model.PushInfoResponse;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class PushInfoResponseTest {
+
+    @Test
+    public void testPushInfoResponseBuilder() {
+
+        UUID one = UUID.randomUUID();
+        UUID two = UUID.randomUUID();
+
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        DateTime dt = formatter.parseDateTime("2013-07-31 21:27:38");
+
+        PushInfoResponse obj = PushInfoResponse.newBuilder()
+                .setPushId(one)
+                .setDirectResponses(4)
+                .setSends(5)
+                .setPushType(PushInfoResponse.PushType.UNICAST_PUSH)
+                .setPushTime("2013-07-31 21:27:38")
+                .setGroupId(two)
+                .build();
+
+        assertNotNull(obj);
+        assertEquals(one, obj.getPushId());
+        assertEquals(4, obj.getDirectResponses());
+        assertEquals(5, obj.getSends());
+        assertEquals(PushInfoResponse.PushType.UNICAST_PUSH, obj.getPushType());
+        assertEquals(dt, obj.getPushTime());
+        assertTrue(obj.getGroupID().isPresent());
+        assertEquals(two, obj.getGroupID().get());
+
+    }
+
+    @Test
+    public void testPushInfoResponseBuilderNoGroupID() {
+
+        UUID one = UUID.randomUUID();
+
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        DateTime dt = formatter.parseDateTime("2013-07-31 21:27:38");
+
+        PushInfoResponse obj = PushInfoResponse.newBuilder()
+                .setPushId(one)
+                .setSends(5)
+                .setPushType(PushInfoResponse.PushType.UNICAST_PUSH)
+                .setPushTime("2013-07-31 21:27:38")
+                .build();
+
+        assertNotNull(obj);
+        assertEquals(one, obj.getPushId());
+        assertEquals(5, obj.getSends());
+        assertEquals(PushInfoResponse.PushType.UNICAST_PUSH, obj.getPushType());
+        assertEquals(dt, obj.getPushTime());
+        assertFalse(obj.getGroupID().isPresent());
+
+    }
+}
--- src/test/java/com/urbanairship/api/reports/PushListingResponseTest.java
+++ src/test/java/com/urbanairship/api/reports/PushListingResponseTest.java
@@ -1,70 +1,51 @@
-/*
- * Copyright (c) 2013-2014.  Urban Airship and Contributors
- */
-
 package com.urbanairship.api.reports;
 
 
+import com.urbanairship.api.reports.model.PushInfoResponse;
 import com.urbanairship.api.reports.model.PushListingResponse;
-import com.urbanairship.api.reports.parse.ReportsObjectMapper;
-import org.codehaus.jackson.map.ObjectMapper;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
+
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class PushListingResponseTest {
 
-    ObjectMapper mapper = ReportsObjectMapper.getInstance();
-
-    String fiveresponse = "{  \n" +
-            "  \"next_page\":\"Value for Next Page\",\n" +
-            "  \"pushes\":[  \n" +
-            "    {  \n" +
-            "      \"push_uuid\":\"df31cae0-fa3c-11e2-97ce-14feb5d317b8\",\n" +
-            "      \"push_time\":\"2013-07-31 23:56:52\",\n" +
-            "      \"push_type\":\"BROADCAST_PUSH\",\n" +
-            "      \"direct_responses\":0,\n" +
-            "      \"sends\":1\n" +
-            "    },\n" +
-            "    {  \n" +
-            "      \"push_uuid\":\"3043779a-fa3c-11e2-a22b-d4bed9a887d4\",\n" +
-            "      \"push_time\":\"2013-07-31 23:51:58\",\n" +
-            "      \"push_type\":\"BROADCAST_PUSH\",\n" +
-            "      \"direct_responses\":0,\n" +
-            "      \"sends\":1\n" +
-            "    },\n" +
-            "    {  \n" +
-            "      \"push_uuid\":\"1c06d01a-fa3c-11e2-aa2d-d4bed9a88699\",\n" +
-            "      \"push_time\":\"2013-07-31 23:51:24\",\n" +
-            "      \"push_type\":\"BROADCAST_PUSH\",\n" +
-            "      \"direct_responses\":0,\n" +
-            "      \"sends\":1\n" +
-            "    },\n" +
-            "    {  \n" +
-            "      \"push_uuid\":\"a50eb7de-fa3b-11e2-912f-90e2ba025998\",\n" +
-            "      \"push_time\":\"2013-07-31 23:48:05\",\n" +
-            "      \"push_type\":\"BROADCAST_PUSH\",\n" +
-            "      \"direct_responses\":0,\n" +
-            "      \"sends\":1\n" +
-            "    },\n" +
-            "    {  \n" +
-            "      \"push_uuid\":\"90483c8a-fa3b-11e2-92d0-90e2ba0253a0\",\n" +
-            "      \"push_time\":\"2013-07-31 23:47:30\",\n" +
-            "      \"push_type\":\"BROADCAST_PUSH\",\n" +
-            "      \"direct_responses\":0,\n" +
-            "      \"sends\":1\n" +
-            "    }\n" +
-            "  ]\n" +
-            "}";
-
     @Test
-    public void testPushInfoList() throws Exception {
-        PushListingResponse response = mapper.readValue(fiveresponse, PushListingResponse.class);
+    public void testPushListingResponse() {
+
+        UUID one = UUID.randomUUID();
+        UUID two = UUID.randomUUID();
+
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        DateTime dt = formatter.parseDateTime("2013-07-31 21:27:38");
+
+        PushInfoResponse obj = PushInfoResponse.newBuilder()
+                .setPushId(one)
+                .setDirectResponses(4)
+                .setSends(5)
+                .setPushType(PushInfoResponse.PushType.UNICAST_PUSH)
+                .setPushTime("2013-07-31 21:27:38")
+                .setGroupId(two)
+                .build();
+
+        PushListingResponse response = PushListingResponse.newBuilder()
+                .setNextPage("The Next Page")
+                .addPushInfoObject(obj)
+                .addPushInfoObject(obj)
+                .addPushInfoObject(obj)
+                .addPushInfoObject(obj)
+                .addPushInfoObject(obj)
+                .build();
+
         assertNotNull(response);
         assertEquals(5, response.getPushInfoList().get().size());
-        assertEquals("Value for Next Page", response.getNextPage().get());
-        assertEquals("1c06d01a-fa3c-11e2-aa2d-d4bed9a88699",
-                response.getPushInfoList().get().get(2).getPushId().toString());
+        assertEquals("The Next Page", response.getNextPage().get());
+        assertEquals(one, response.getPushInfoList().get().get(2).getPushId());
+        assertEquals(two, response.getPushInfoList().get().get(2).getGroupID().get());
     }
 }
