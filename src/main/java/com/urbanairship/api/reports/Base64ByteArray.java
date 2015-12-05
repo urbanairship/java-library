9892c2cbbe034a4dd994350059f2327f804a1852
Dejan Vukmirovic <deyo.vuk@gmail.com>
2015-12-06 00:24:37 +0100

    Upgrading Jersey library to v2.19

--- src/main/java/com/urbanairship/api/reports/Base64ByteArray.java
+++ src/main/java/com/urbanairship/api/reports/Base64ByteArray.java
@@ -4,19 +4,23 @@
 
 package com.urbanairship.api.reports;
 
-import com.google.common.base.Preconditions;
-import com.sun.jersey.core.util.Base64;
-
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.glassfish.jersey.internal.util.Base64;
+
+import com.google.common.base.Preconditions;
 
 public final class Base64ByteArray {
 
     public byte[] binary;
 
     public Base64ByteArray(String value) {
-        Preconditions.checkArgument(Base64.isBase64(value));
+            	
+    	Preconditions.checkArgument(checkForEncode(value));
 
-        this.binary = Base64.decode(value);
+        this.binary = Base64.decode(value.getBytes());
     }
 
     public byte[] getByteArray() {
@@ -32,6 +36,21 @@ public final class Base64ByteArray {
         return new String(binary);
     }
 
+    /**
+     * Internal implementation of check is required as Jersey 2.x no longer has method Base64.isBase64().
+     * As a transient dependency Apache Commons library is providing it's Base64 implementation with appropriate method for checks,
+     * but this library can produce false positives as it treats whitespaces as positives in evaluation.
+     * 
+     * @param string Value to be checked.
+     * @return Flag indicating if input string is Base64 encoded.
+     */
+    private boolean checkForEncode(String string) {
+        String pattern = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(string);
+        return m.find();
+    }
+    
     // Does not use Guava::Objects, gives inconsistent results with byte array
     @Override
     public boolean equals(Object o) {
