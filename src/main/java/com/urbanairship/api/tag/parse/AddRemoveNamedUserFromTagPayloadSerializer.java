4cc28e270ff96cb204c1c41bbfd72d3c1908bce3
Michael Ball <mwball@gmail.com>
2016-02-17 17:39:33 -0500

    fix serializer to be 1.5 compatible

--- src/main/java/com/urbanairship/api/tag/parse/AddRemoveNamedUserFromTagPayloadSerializer.java
+++ src/main/java/com/urbanairship/api/tag/parse/AddRemoveNamedUserFromTagPayloadSerializer.java
@@ -4,13 +4,13 @@
 
 package com.urbanairship.api.tag.parse;
 
-import com.urbanairship.api.tag.model.AddRemoveDeviceFromTagPayload;
 import com.urbanairship.api.tag.model.AddRemoveNamedUserFromTagPayload;
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializerProvider;
 
 import java.io.IOException;
+import java.util.List;
 
 public final class AddRemoveNamedUserFromTagPayloadSerializer extends JsonSerializer<AddRemoveNamedUserFromTagPayload> {
 
@@ -23,45 +23,43 @@ public final class AddRemoveNamedUserFromTagPayloadSerializer extends JsonSerial
         jgen.writeArrayFieldStart(payload.getAudienceName());
 
         for (String s : payload.getAudience()) {
-                    jgen.writeString(s);
-                }
+            jgen.writeString(s);
+        }
         jgen.writeEndArray();
         jgen.writeEndObject();
 
 
         if (payload.getAddedTags() != null) {
             jgen.writeObjectFieldStart("add");
-            payload.getAddedTags().forEach((k, v) -> {
-                try {
+
+            for (String k : payload.getAddedTags().keySet()) {
+                List<String> v = payload.getAddedTags().get(k);
+
                 jgen.writeArrayFieldStart(k);
 
-                    for (String s : v) {
-                        jgen.writeString(s);
-                    }
-                jgen.writeEndArray();
-
-                } catch (IOException io){
-
-                }
-            });
-            jgen.writeEndObject();
-        }
-        if (payload.getRemovedTags() != null) {
-            jgen.writeObjectFieldStart("remove");
-            payload.getRemovedTags().forEach((k, v) -> {
-                try{
-                    jgen.writeArrayFieldStart(k);
                 for (String s : v) {
                     jgen.writeString(s);
                 }
                 jgen.writeEndArray();
-            } catch (IOException io){
+            }
+            jgen.writeEndObject();
+        }
+        if (payload.getRemovedTags() != null) {
+            jgen.writeObjectFieldStart("remove");
+            for (String k : payload.getRemovedTags().keySet()) {
+                List<String> v = payload.getRemovedTags().get(k);
+
+                jgen.writeArrayFieldStart(k);
+                for (String s : v) {
+                    jgen.writeString(s);
+                }
+                jgen.writeEndArray();
 
             }
-            });
             jgen.writeEndObject();
         }
 
         jgen.writeEndObject();
+
     }
 }
