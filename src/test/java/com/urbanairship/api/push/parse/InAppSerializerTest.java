acb44f8204b20d882dc6675b068ae7286f29d0ac
SeanyC <sean.conlin@urbanairship.com>
2017-02-14 16:26:10 -0800

    Changed In-App serliazer on expiry to DateTime instead of String.

--- src/test/java/com/urbanairship/api/push/parse/InAppSerializerTest.java
+++ src/test/java/com/urbanairship/api/push/parse/InAppSerializerTest.java
@@ -5,6 +5,7 @@ import com.urbanairship.api.push.model.Display;
 import com.urbanairship.api.push.model.InApp;
 import com.urbanairship.api.push.model.Position;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -34,9 +35,12 @@ public class InAppSerializerTest {
                 .setPosition(Position.TOP)
                 .build();
 
+        DateTime expiry = new DateTime(2017, 4, 15, 11, 30);
+
         InApp inApp = InApp.newBuilder()
                 .setAlert("test alert")
                 .setDisplay(display)
+                .setExpiry(expiry)
                 .build();
 
         String parsedJson = MAPPER.writeValueAsString(inApp);
