e6b45d08cf1d2d27b4211f553991cb540a47cdd3
Jared Teems <jared.teems@adjenti.com>
2017-03-15 10:02:40 -0400

    Add batch api support
    Fixes #14

---
+++ src/main/java/com/urbanairship/api/push/BatchPushRequest.java
@@ -0,0 +1,122 @@
+package com.urbanairship.api.push;
+
+/*
+ * Copyright (c) 2013-2016.  Urban Airship and Contributors
+ */
+
+import com.google.common.base.Preconditions;
+import com.google.common.net.HttpHeaders;
+import com.ning.http.util.StringUtils;
+import com.urbanairship.api.client.Request;
+import com.urbanairship.api.client.RequestUtils;
+import com.urbanairship.api.client.ResponseParser;
+import com.urbanairship.api.push.model.PushPayload;
+import com.urbanairship.api.push.model.PushResponse;
+import com.urbanairship.api.push.parse.PushObjectMapper;
+import org.apache.http.entity.ContentType;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * The BatchPushRequest class builds push and push validation requests to be executed in
+ * the {@link com.urbanairship.api.client.UrbanAirshipClient}.
+ */
+public class BatchPushRequest implements Request<PushResponse> {
+
+    private final static String API_PUSH_PATH = "/api/push/";
+    private final static String API_VALIDATE_PATH = "/api/push/validate/";
+
+    private final List<PushPayload> payload = new ArrayList<>();
+    private boolean validateOnly;
+
+    private BatchPushRequest(PushPayload payload) {
+        Preconditions.checkNotNull(payload, "Payload required when creating a push request");
+        this.payload.add(payload);
+    }
+
+    /**
+     * Create a push request.
+     *
+     * @param payload PushPayload
+     * @return BatchPushRequest
+     */
+    public static BatchPushRequest newRequest(PushPayload payload) {
+        return new BatchPushRequest(payload);
+    }
+
+    /**
+     * Add additional payloads to a batch push request
+     * @param newPayload
+     * @return BatchPushRequest
+     */
+    public BatchPushRequest addRequest(PushPayload newPayload) {
+        Preconditions.checkNotNull(payload, "Payload required when adding to a push request");
+        payload.add(newPayload);
+        return this;
+    }
+
+    /**
+     * Sets if the request should only validate the payload.
+     *
+     * @param validateOnly {@code true} to only validate the payload, {@code false} to send the push request.
+     * @return The batch push request.
+     */
+    public BatchPushRequest setValidateOnly(boolean validateOnly) {
+        this.validateOnly = validateOnly;
+        return this;
+    }
+
+    @Override
+    public ContentType getContentType() {
+        return ContentType.APPLICATION_JSON;
+    }
+
+    @Override
+    public Map<String, String> getRequestHeaders() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE_JSON);
+        headers.put(HttpHeaders.ACCEPT, UA_VERSION_JSON);
+        return headers;
+    }
+
+    @Override
+    public HttpMethod getHttpMethod() {
+        return HttpMethod.POST;
+    }
+
+    @Override
+    public String getRequestBody() {
+        StringBuilder sb = new StringBuilder("[");
+
+        for (PushPayload pushPayload : this.payload) {
+            sb.append(pushPayload.toJSON());
+            sb.append(",");
+        }
+        // remove last comma
+        sb.setLength(sb.length() - 1);
+
+        return sb.append("]").toString();
+    }
+
+    @Override
+    public URI getUri(URI baseUri) {
+        String path  = validateOnly ? API_VALIDATE_PATH : API_PUSH_PATH;
+        return RequestUtils.resolveURI(baseUri, path);
+    }
+
+    @Override
+    public ResponseParser<PushResponse> getResponseParser() {
+        return new ResponseParser<PushResponse>() {
+            @Override
+            public PushResponse parse(String response) throws IOException {
+                return PushObjectMapper.getInstance().readValue(response, PushResponse.class);
+            }
+        };
+    }
+}
---
+++ src/test/java/com/urbanairship/api/push/BatchPushRequestTest.java
@@ -0,0 +1,104 @@
+package com.urbanairship.api.push;
+
+import com.google.common.net.HttpHeaders;
+import com.urbanairship.api.client.Request;
+import com.urbanairship.api.client.ResponseParser;
+import com.urbanairship.api.push.model.DeviceType;
+import com.urbanairship.api.push.model.DeviceTypeData;
+import com.urbanairship.api.push.model.PushPayload;
+import com.urbanairship.api.push.model.PushResponse;
+import com.urbanairship.api.push.model.audience.Selectors;
+import com.urbanairship.api.push.model.notification.Notifications;
+import com.urbanairship.api.push.parse.PushObjectMapper;
+import org.apache.http.entity.ContentType;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class BatchPushRequestTest {
+
+    PushPayload payload = PushPayload.newBuilder()
+        .setAudience(Selectors.all())
+        .setDeviceTypes(DeviceTypeData.of(DeviceType.IOS))
+        .setNotification(Notifications.alert("Foo"))
+        .build();
+
+    PushPayload payload2 = PushPayload.newBuilder()
+        .setAudience(Selectors.all())
+        .setDeviceTypes(DeviceTypeData.of(DeviceType.IOS))
+        .setNotification(Notifications.alert("Bar"))
+        .build();
+
+    BatchPushRequest pushRequest = BatchPushRequest.newRequest(payload).addRequest(payload2);
+    BatchPushRequest validateRequest = BatchPushRequest.newRequest(payload).addRequest(payload2).setValidateOnly(true);
+
+    @Test
+    public void testContentType() throws Exception {
+        assertEquals(pushRequest.getContentType(), ContentType.APPLICATION_JSON);
+        assertEquals(validateRequest.getContentType(), ContentType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void testMethod() throws Exception {
+        assertEquals(pushRequest.getHttpMethod(), Request.HttpMethod.POST);
+        assertEquals(validateRequest.getHttpMethod(), Request.HttpMethod.POST);
+    }
+
+    @Test
+    public void testBody() throws Exception {
+        assertEquals(pushRequest.getRequestBody(), "[{\"audience\":\"ALL\",\"device_types\":[\"ios\"],\"notification\":{\"alert\":\"Foo\"}},{\"audience\":\"ALL\",\"device_types\":[\"ios\"],\"notification\":{\"alert\":\"Bar\"}}]");
+        assertEquals(validateRequest.getRequestBody(), "[{\"audience\":\"ALL\",\"device_types\":[\"ios\"],\"notification\":{\"alert\":\"Foo\"}},{\"audience\":\"ALL\",\"device_types\":[\"ios\"],\"notification\":{\"alert\":\"Bar\"}}]");
+    }
+
+    @Test
+    public void testHeaders() throws Exception {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(HttpHeaders.CONTENT_TYPE, Request.CONTENT_TYPE_JSON);
+        headers.put(HttpHeaders.ACCEPT, Request.UA_VERSION_JSON);
+
+        assertEquals(pushRequest.getRequestHeaders(), headers);
+        assertEquals(validateRequest.getRequestHeaders(), headers);
+    }
+
+    @Test
+    public void testURI() throws Exception {
+        URI baseURI = URI.create("https://go.urbanairship.com");
+
+        URI expextedURI = URI.create("https://go.urbanairship.com/api/push/");
+        assertEquals(pushRequest.getUri(baseURI), expextedURI);
+
+        expextedURI = URI.create("https://go.urbanairship.com/api/push/validate/");
+        assertEquals(validateRequest.getUri(baseURI), expextedURI);
+    }
+
+    @Test
+    public void testPushParser() throws Exception {
+        ResponseParser responseParser = new ResponseParser<PushResponse>() {
+            @Override
+            public PushResponse parse(String response) throws IOException {
+                return PushObjectMapper.getInstance().readValue(response, PushResponse.class);
+            }
+        };
+
+        String response = "{\"ok\" : true,\"operation_id\" : \"df6a6b50\", \"push_ids\":[\"PushID\"]}";
+        assertEquals(pushRequest.getResponseParser().parse(response), responseParser.parse(response));
+    }
+
+    @Test
+    public void testPushValidationParser() throws Exception {
+        ResponseParser responseParser = new ResponseParser<PushResponse>() {
+            @Override
+            public PushResponse parse(String response) throws IOException {
+                return PushObjectMapper.getInstance().readValue(response, PushResponse.class);
+            }
+        };
+        
+        String response = "{\"ok\" : true}";
+        assertEquals(validateRequest.getResponseParser().parse(response), responseParser.parse(response));
+    }
+}
