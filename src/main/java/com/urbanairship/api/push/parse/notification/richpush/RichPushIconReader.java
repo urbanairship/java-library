2bd081d7667390fa87f8bc5e02b9289604a425eb
Burak Amasyalı <bamasyali@gmail.com>
2017-05-04 11:25:43 +0300

    Icon support for RichPushMessage

---
+++ src/main/java/com/urbanairship/api/push/parse/notification/richpush/RichPushIconReader.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013-2016.  Urban Airship and Contributors
+ */
+
+package com.urbanairship.api.push.parse.notification.richpush;
+
+import com.urbanairship.api.common.parse.APIParsingException;
+import com.urbanairship.api.common.parse.JsonObjectReader;
+import com.urbanairship.api.common.parse.StringFieldDeserializer;
+import com.urbanairship.api.push.model.notification.richpush.RichPushIcon;
+import org.codehaus.jackson.JsonParser;
+
+import java.io.IOException;
+
+public class RichPushIconReader implements JsonObjectReader<RichPushIcon> {
+
+    private RichPushIcon.Builder builder = RichPushIcon.newBuilder();
+
+    public RichPushIconReader() {
+    }
+
+    public void readListIcon(JsonParser parser) throws IOException {
+        builder.setListIcon(StringFieldDeserializer.INSTANCE.deserialize(parser, "list_icon"));
+    }
+
+    @Override
+    public RichPushIcon validateAndBuild() throws IOException {
+        try {
+            return builder.build();
+        } catch (Exception e) {
+            throw new APIParsingException(e.getMessage(), e);
+        }
+    }
+}
