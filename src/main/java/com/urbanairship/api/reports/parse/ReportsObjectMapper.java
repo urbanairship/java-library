abf1b7b4d396cc776c5959d115628ede9193e99c
jenniferjsmmiller <jenniferjsmmiller@gmail.com>
2017-05-14 13:14:43 -0700

    Removed references to RichPerPush reports

--- src/main/java/com/urbanairship/api/reports/parse/ReportsObjectMapper.java
+++ src/main/java/com/urbanairship/api/reports/parse/ReportsObjectMapper.java
@@ -9,7 +9,6 @@ import com.urbanairship.api.reports.model.PlatformStats;
 import com.urbanairship.api.reports.model.PlatformStatsResponse;
 import com.urbanairship.api.reports.model.PushInfoResponse;
 import com.urbanairship.api.reports.model.PushListingResponse;
-import com.urbanairship.api.reports.model.RichPerPushCounts;
 import com.urbanairship.api.reports.model.StatisticsResponse;
 import org.codehaus.jackson.Version;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -26,7 +25,6 @@ public class ReportsObjectMapper {
         MODULE.addDeserializer(StatisticsResponse.class, new StatisticsResponseDeserializer());
         MODULE.addDeserializer(PlatformStats.class, new PlatformStatsDeserializer());
         MODULE.addDeserializer(PlatformStatsResponse.class, new PlatformStatsResponseDeserializer());
-        MODULE.addDeserializer(RichPerPushCounts.class, new RichPerPushCountsDeserializer());
 
         MAPPER.registerModule(MODULE);
         MAPPER.registerModule(PushObjectMapper.getModule());
