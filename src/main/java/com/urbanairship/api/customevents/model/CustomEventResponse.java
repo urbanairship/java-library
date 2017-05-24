b6c4080ff76a5d4e3eafb949b276893a314b616f
Devin Smythe <devin.smythe@urbanairshp.com>
2017-05-24 16:38:58 -0700

    Added additonal testing and hashCode and equals methods to the custom events classes

--- src/main/java/com/urbanairship/api/customevents/model/CustomEventResponse.java
+++ src/main/java/com/urbanairship/api/customevents/model/CustomEventResponse.java
@@ -1,5 +1,6 @@
 package com.urbanairship.api.customevents.model;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 
 public class CustomEventResponse {
@@ -22,10 +23,32 @@ public class CustomEventResponse {
         return operationId;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CustomEventResponse that = (CustomEventResponse) o;
+
+        return ok == that.ok &&
+                Objects.equal(operationId, that.operationId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(operationId, ok);
+    }
+
     /**
      * Get the response status as a boolean
      * @return Response status
      */
+
     public boolean isOk() {
         return ok;
     }
