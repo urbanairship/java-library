514da4654b65fa6759a420db6d968016c3392a0a
jenniferjsmmiller <jenniferjsmmiller@gmail.com>
2017-05-25 15:55:32 -0700

    Adding A/B tests

--- src/main/java/com/urbanairship/api/push/model/Experiment.java
+++ src/main/java/com/urbanairship/api/push/model/Experiment.java
@@ -1,12 +1,18 @@
 /*
- * Copyright (c) 2013-2016.  Urban Airship and Contributors
+ * Copyright (c) 2013-2017.  Urban Airship and Contributors
  */
 
 package com.urbanairship.api.push.model;
 
 import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang.StringUtils;
+import com.urbanairship.api.push.model.notification.Notification;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -41,19 +47,153 @@ public final class Experiment {
 
     @Override
     public boolean equals(Object obj) {
-        return false;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Experiment other = (Experiment) obj;
+
+        if (experimentPayload != null ? !experimentPayload.equals(other.experimentPayload) : other.experimentPayload != null) {
+            return false;
+        }
+        if (variants != null ? !variants.equals(other.variants) : other.variants != null) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public String toString() {
-        return null;
+        return "Experiment{" +
+                "experimentPayload='" + experimentPayload + '\'' +
+                ", variants=" + variants +
+                '}';
+
     }
 
     public static class Builder {
 
+        private String experimentPayload;
+        private List<Variant> variants;
+
+        public Builder setExperimentPayload(String experimentPayload) {
+            this.experimentPayload = experimentPayload;
+            return this;
+        }
+
+        public <V extends Variant> Builder addVariant(V variant) {
+            if (variants == null) {
+                variants = Lists.newArrayList();
+            }
+
+            variants.add(variant);
+            return this;
+        }
+
+        public Builder addAllVariants(List<? extends Variant> variants) {
+            this.variants = Lists.newArrayList(variants);
+            return this;
+        }
+
+        public Experiment build() {
+            Preconditions.checkArgument(StringUtils.isNotEmpty(experimentPayload),
+                    "An experiment payload must be provided.");
+            Preconditions.checkNotNull(variants, "An experiment requires at least one variant.");
+            Preconditions.checkArgument(variants.size() > 0, "At least one variant must be present.");
+
+            return new Experiment(experimentPayload, ImmutableList.copyOf(variants));
+        }
     }
 
     public static class Variant {
+
+        private int id;
+        private Optional<BigDecimal> weight;
+        private final Notification notification;
+
+        private Variant(int id,  Optional<BigDecimal> weight, Notification notification) {
+            this.id = id;
+            this.weight = weight;
+            this.notification = notification;
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        /**
+         * ID of the variant. Should be unique within the experiment.
+         *
+         * @return
+         */
+        public int getId() {
+            return id;
+        }
+
+        public  Optional<BigDecimal> getWeight() {
+            return weight;
+        }
+
+        public Notification getNotification() {
+            return notification;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(id, weight, notification);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            final Variant other = (Variant) obj;
+            return Objects.equal(this.id, other.id)
+                    && Objects.equal(this.weight, other.weight)
+                    && Objects.equal(this.notification, other.notification);
+        }
+
+        @Override
+        public String toString() {
+            return "Variant{" +
+                    "id=" + id +
+                    ", notification=" + notification +
+                    ", weight=" + weight.get() +
+                    '}';
+        }
+
+        public static class Builder {
+
+            private Integer id;
+            private Optional<BigDecimal> weight;
+            private Notification notification;
+
+            public Builder setId(int id) {
+                this.id = id;
+                return this;
+            }
+
+            public Builder setNotification(Notification notification) {
+                this.notification = notification;
+                return this;
+            }
+
+            public Variant build() {
+                Preconditions.checkNotNull(id, "An ID must be provided.");
+                Preconditions.checkNotNull(notification, "A notification must be provided.");
+                Preconditions.checkNotNull(weight.get(), "A weight must be provided.");
+
+
+                return new Variant(id, weight, notification);
+            }
+        }
 
     }
 }
