e37606e7a16d7b63c8f2cc260acbbf7f88f2571c
Devin Smythe <devin.smythe@urbanairshp.com>
2018-02-08 11:28:40 -0800

    Updated the auth and added updated the tests

--- src/main/java/com/urbanairship/api/client/RequestClient.java
+++ src/main/java/com/urbanairship/api/client/RequestClient.java
@@ -9,9 +9,5 @@ import java.util.concurrent.Future;
 public interface RequestClient {
     public <T> Future<Response> executeAsync(final Request<T> request, final ResponseCallback callback, Map<String, String> headers);
 
-    public String getAppKey();
-    public Optional<String> getAppSecret();
-    public Optional<String> getBearerToken();
-
     public void close() throws IOException;
 }
