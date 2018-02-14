afd700d70aa01efca602cb701e155154fd32cc99
Devin Smythe <devin.smythe@urbanairshp.com>
2018-02-13 16:15:13 -0800

    Added some more docs to RequestClient

--- src/main/java/com/urbanairship/api/client/RequestClient.java
+++ src/main/java/com/urbanairship/api/client/RequestClient.java
@@ -8,5 +8,12 @@ import java.util.concurrent.Future;
  * RequestClient must be implemented to create a custom HTTP client that is then used by UrbanAirshipClient to make requests.
  */
 public interface RequestClient extends Closeable {
+    /**
+     * Executes the HTTP request.
+     * @param request the Request.
+     * @param callback the ResponseCallback.
+     * @param headers a Map of the headers.
+     * @return The Future Response.
+     */
     public <T> Future<Response> executeAsync(final Request<T> request, final ResponseCallback callback, Map<String, String> headers);
 }
