d2541efceebc8191928b6fdaf5f36bc3772742f8
agoodman42 <aaron.goodman@urbanairship.com>
2018-10-23 14:58:37 -0700

    created registerEmailChannel. Now we have to serialize it properly

--- src/main/java/com/urbanairship/api/channel/model/email/OptInLevel.java
+++ src/main/java/com/urbanairship/api/channel/model/email/OptInLevel.java
@@ -1,4 +1,39 @@
 package com.urbanairship.api.channel.model.email;
 
-public class OptInLevel {
+import com.google.common.base.Optional;
+
+/**
+ * Enum of opt in levels
+ */
+public enum OptInLevel {
+
+    COMMERCIAL("commercial"),
+    TRANSACTIONAL("transactional"),
+    NONE("none");
+
+    private final String identifier;
+
+    private OptInLevel() {
+        this(null);
+    }
+
+    private OptInLevel(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public static Optional<OptInLevel> find(String identifier) {
+        for (OptInLevel optInLevel : values()) {
+            if (optInLevel.getIdentifier().equals(identifier)) {
+                return Optional.of(optInLevel);
+            }
+        }
+
+        return Optional.absent();
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
 }
+
