58321663e9d201eb0df0077ffb8cafbd2ff6f6cd
Harvey Rogers <harveyr@gmail.com>
2018-11-06 09:39:32 -0800

    dont think we need latest after all

--- ci/upload_docs.sh
+++ ci/upload_docs.sh
@@ -4,12 +4,10 @@ set -e
 
 # https://stackoverflow.com/a/4774063
 BASE_PATH="$( cd "$(dirname "$0")/.." ; pwd -P )"
-
 VERSION="$(cat VERSION.txt | tr -d '[:space:]')"
 DOCS_PATH="${BASE_PATH}/target/site/apidocs"
 TARBALL="${VERSION}.tar.gz"
-VERSIONED_DEST="gs://ua-web-ci-library-docs/libraries/java/$TARBALL"
-LATEST_DEST="gs://ua-web-ci-library-docs/libraries/java/latest.tar.gz"
+DEST="gs://ua-web-ci-library-docs/libraries/java/$TARBALL"
 
 if [ ! -d "${DOCS_PATH}" ]; then
   echo "Error: ${DOCS_PATH} does not exist or is not a directory"
@@ -19,13 +17,9 @@ fi
 cd $DOCS_PATH
 tar -czf $TARBALL *
 
-set -x
-
-# FIXME:
-
-# if ["${BRANCH_NAME}" = "master"]; then
-  gsutil cp $TARBALL gs://ua-web-ci-library-docs/libraries/java/$TARBALL
-  gsutil cp $VERSIONED_DEST $LATEST_DEST
-# else
-#   echo "Not uploading tarball on branch ${BRANCH_NAME}"
-# fi
+if ["${BRANCH_NAME}" = "master"]; then
+  set -x
+  gsutil cp $TARBALL $DEST
+else
+  echo "Not uploading tarball on branch ${BRANCH_NAME}"
+fi
