be5ae93abbc09a955b2353a08272fcd6bd486b21
Devin Smythe <devin@urbanairship.com>
2018-12-07 10:36:18 -0800

    Added docs for sms audience

--- src/main/java/com/urbanairship/api/createandsend/model/audience/sms/SmsChannel.java
+++ src/main/java/com/urbanairship/api/createandsend/model/audience/sms/SmsChannel.java
@@ -8,6 +8,9 @@ import org.joda.time.DateTime;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Represents a single SmsChannel for create and send audience.
+ */
 public final class SmsChannel {
     private final String msisdn;
     private final DateTime optedIn;
@@ -27,22 +30,43 @@ public final class SmsChannel {
         }
     }
 
+    /**
+     * Builder for SmsChannel
+     * @return Builder
+     */
     public static Builder newBuilder() {
         return new Builder();
     }
 
+    /**
+     * Get the msisdn. The address that you want to populate the channel's address field.
+     * @return String
+     */
     public String getMsisdn() {
         return msisdn;
     }
 
+    /**
+     * Get the date when the user opted in.
+     * @return DateTime
+     */
     public DateTime getOptedIn() {
         return optedIn;
     }
 
+    /**
+     * Get the sender. The address that you want to populate the channel's address field.
+     * @return String
+     */
     public String getSender() {
         return sender;
     }
 
+    /**
+     * Get the substitutions. Additional key-value pairs representing variables your notification template.
+     * Your variable keys must not be prefixed with ua_
+     * @return Optional ImmutableMap of Strings.
+     */
     public Optional<ImmutableMap<String, String>> getSubstitutions() {
         return substitutions;
     }
@@ -80,26 +104,54 @@ public final class SmsChannel {
 
         private ImmutableMap.Builder<String, String> substitutions = ImmutableMap.builder();
 
+        /**
+         * Set the msisdn. The address that you want to populate the channel's address field.
+         * @param msisdn String
+         * @return SmsChannel Builder
+         */
         public Builder setMsisdn(String msisdn) {
             this.msisdn = msisdn;
             return this;
         }
 
+        /**
+         * Set the date when the user opted in.
+         * @param optedIn DateTime
+         * @return SmsChannel Builder
+         */
         public Builder setOptedIn(DateTime optedIn) {
             this.optedIn = optedIn;
             return this;
         }
 
+        /**
+         * Set the sender. The address that you want to populate the channel's address field.
+         * @param sender String
+         * @return SmsChannel Builder
+         */
         public Builder setSender(String sender) {
             this.sender = sender;
             return this;
         }
 
+        /**
+         * Add a substitution. Additional key-value pairs representing variables your notification template.
+         * Your variable keys must not be prefixed with ua_
+         * @param key String
+         * @param value String
+         * @return SmsChannel Builder
+         */
         public Builder addSubstitution(String key, String value) {
             substitutions.put(key, value);
             return this;
         }
 
+        /**
+         * Add all substitutions. Additional key-value pairs representing variables your notification template.
+         * Your variable keys must not be prefixed with ua_
+         * @param substitutions Map of Strings
+         * @return SmsChannel Builder
+         */
         public Builder addAllSubstitutions(Map<String, String> substitutions) {
             substitutions.putAll(substitutions);
             return this;
--- src/main/java/com/urbanairship/api/createandsend/model/audience/sms/SmsChannels.java
+++ src/main/java/com/urbanairship/api/createandsend/model/audience/sms/SmsChannels.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.List;
 
+/**
+ * Contains all SmsChannel objects to be used in the create and send audience.
+ */
 public class SmsChannels {
     private final ImmutableList<SmsChannel> smsChannels;
 
@@ -11,10 +14,18 @@ public class SmsChannels {
         this.smsChannels = builder.smsChannels.build();
     }
 
+    /**
+     * Builder for SmsChannels
+     * @return Builder
+     */
     public static Builder newBuilder() {
         return new Builder();
     }
 
+    /**
+     * Get all the audience Sms channels.
+     * @return ImmutableList of SmsChannel
+     */
     public ImmutableList<SmsChannel> getSmsChannels() {
         return smsChannels;
     }
@@ -22,11 +33,21 @@ public class SmsChannels {
     public static class Builder {
         private ImmutableList.Builder<SmsChannel> smsChannels = ImmutableList.builder();
 
+        /**
+         * Add a SmsChannel for create and send audience.
+         * @param smsChannel SmsChannel
+         * @return SmsChannels Builder
+         */
         public Builder addSmsChannel(SmsChannel smsChannel) {
             smsChannels.add(smsChannel);
             return this;
         }
 
+        /**
+         * Add all SmsChannel objects for create and send audience.
+         * @param smsChannelList SmsChannel
+         * @return SmsChannels Builder
+         */
         public Builder addAllSmsChannels(List<SmsChannel> smsChannelList) {
             smsChannels.addAll(smsChannelList);
             return this;
