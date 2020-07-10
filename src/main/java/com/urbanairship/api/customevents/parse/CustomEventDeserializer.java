6f562b4421d155b87b8b46ff1ae463777d5c225d
agoodman42 <aaron.goodman@urbanairship.com>
2020-07-10 15:04:07 -0700

    undid typo fixes for deleted files to prevent merge conflicts. Changed CustomEventUserSerializer to add namedUser without needing a hashmap.

--- src/main/java/com/urbanairship/api/customevents/parse/CustomEventDeserializer.java
+++ src/main/java/com/urbanairship/api/customevents/parse/CustomEventDeserializer.java
@@ -17,10 +17,10 @@ public class CustomEventDeserializer extends JsonDeserializer<CustomEventPayload
 
     private static final FieldParserRegistry<CustomEventPayload, CustomEventReader> FIELD_PARSERS = new MapFieldParserRegistry<CustomEventPayload, CustomEventReader>(
             ImmutableMap.<String, FieldParser<CustomEventReader>>builder()
-            .put("occurred", new FieldParser<CustomEventReader>() {
+            .put("occured", new FieldParser<CustomEventReader>() {
                 @Override
                 public void parse(CustomEventReader reader, JsonParser json, DeserializationContext context) throws IOException {
-                    reader.readOccurred(json);
+                    reader.readOccured(json);
                 }
             })
             .put("user", new FieldParser<CustomEventReader>() {
