c62e0c9700e5b439bf9d685c4470077f0197650b
agoodman42 <aaron.goodman@urbanairship.com>
2020-07-23 15:05:58 -0700

    wrote test for array properties based on test in Venus. Got tests to green, and got desired result from scrach test

--- src/test/java/com/urbanairship/api/customevents/model/CustomEventPayloadTest.java
+++ src/test/java/com/urbanairship/api/customevents/model/CustomEventPayloadTest.java
@@ -5,7 +5,9 @@ import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -106,6 +108,82 @@ public class CustomEventPayloadTest {
         );
         customEventPayload.getCustomEventBody().getProperties().get().forEach(
                 (property, propValue) -> assertTrue(propValue.isPrimitive())
+        );
+    }
+
+    @Test
+    public void testArrayProperties() {
+        CustomEventUser customEventUser = CustomEventUser.newBuilder()
+                .setChannel("e393d28e-23b2-4a22-9ace-dc539a5b07a8")
+                .setCustomEventChannelType(CustomEventChannelType.ANDROID_CHANNEL)
+                .build();
+
+        DateTime occurred = new DateTime(2015, 5, 2, 2, 31, 22, DateTimeZone.UTC);
+
+
+        Map<String,CustomEventPropValue> properties = new HashMap<>();
+
+
+        List<CustomEventPropValue> items = new ArrayList<>();
+        items.add(CustomEventPropValue.of("la croix"));
+        items.add(CustomEventPropValue.of("more lacroix"));
+
+        List<CustomEventPropValue> numbers = new ArrayList<>();
+        numbers.add(CustomEventPropValue.of(1));
+        numbers.add(CustomEventPropValue.of(22.23));
+        numbers.add(CustomEventPropValue.of(0));
+
+        Map<String, CustomEventPropValue> bootsMap = new HashMap<>();
+
+        bootsMap.put("price", CustomEventPropValue.of(40));
+        bootsMap.put("color", CustomEventPropValue.of("black"));
+        bootsMap.put("waterproof", CustomEventPropValue.of(false));
+
+        List<CustomEventPropValue> shoes = new ArrayList<>();
+
+        CustomEventPropValue boots = CustomEventPropValue.of(bootsMap);
+
+        Map<String, CustomEventPropValue> sneakersMap = new HashMap<>();
+
+        sneakersMap.put("price", CustomEventPropValue.of(10));
+        sneakersMap.put("color", CustomEventPropValue.of("red"));
+        sneakersMap.put("clearance", CustomEventPropValue.of(true));
+
+        CustomEventPropValue sneakers = CustomEventPropValue.of(sneakersMap);
+
+        shoes.add(boots);
+        shoes.add(sneakers);
+
+        properties.put("items", CustomEventPropValue.of(items));
+        properties.put("numbers", CustomEventPropValue.of(numbers));
+        properties.put("shoes", CustomEventPropValue.of((Iterable) shoes));
+
+        CustomEventBody customEventBody = CustomEventBody.newBuilder()
+                .setName("purchased")
+                .setSessionId("22404b07-3f8f-4e42-a4ff-a996c18fa9f1")
+                .addAllPropertyEntries(properties)
+                .build();
+
+        CustomEventPayload customEventPayload = CustomEventPayload.newBuilder()
+                .setOccurred(occurred)
+                .setCustomEventBody(customEventBody)
+                .setCustomEventUser(customEventUser)
+                .build();
+
+        assertEquals(customEventPayload.getCustomEventBody().getProperties().get().size(), 3);
+        customEventPayload.getCustomEventBody().getProperties().get().forEach(
+                (property, propValue) -> assertTrue(propValue.isArray())
+        );
+
+        System.out.println(customEventPayload.getCustomEventBody().getProperties().get());
+        customEventPayload.getCustomEventBody().getProperties().get().get("items").getAsList().forEach(
+                 propValue-> assertTrue(propValue.isString())
+        );
+        customEventPayload.getCustomEventBody().getProperties().get().get("numbers").getAsList().forEach(
+                propValue -> assertTrue(propValue.isNumber())
+        );
+        customEventPayload.getCustomEventBody().getProperties().get().get("shoes").getAsList().forEach(
+                propValue -> assertTrue(propValue.isObject())
         );
     }
 }
