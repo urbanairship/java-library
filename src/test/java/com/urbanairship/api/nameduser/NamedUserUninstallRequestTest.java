f8f6d00714db029d681962c6ede994abac004bb6
Martin V <32457411+martin-vct@users.noreply.github.com>
2021-12-22 10:05:02 +0100

    Add Named users uninstall endpoint (#253)

---
+++ src/test/java/com/urbanairship/api/nameduser/NamedUserUninstallRequestTest.java
@@ -0,0 +1,74 @@
+package com.urbanairship.api.nameduser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.urbanairship.api.channel.parse.ChannelObjectMapper;
+
+import org.apache.http.entity.ContentType;
+import org.junit.Test;
+
+import com.google.common.net.HttpHeaders;
+import com.urbanairship.api.client.Request;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class NamedUserUninstallRequestTest {
+
+    private static final ObjectMapper MAPPER = ChannelObjectMapper.getInstance();
+
+    @Test
+    public void testUninstallNamedUser() throws Exception {
+
+        NamedUserUninstallRequest request = NamedUserUninstallRequest
+            .newUninstallRequest(ImmutableList.of("user-id-1234","user-id-5678"));
+
+        String expected = "{\"named_user_id\":[\"user-id-1234\",\"user-id-5678\"]}";
+
+        JsonNode jsonFromObject = MAPPER.readTree(request.getRequestBody());
+        JsonNode jsonFromString = MAPPER.readTree(expected);
+
+        assertEquals(jsonFromObject, jsonFromString);
+    }
+
+
+    NamedUserUninstallRequest request = NamedUserUninstallRequest
+    .newUninstallRequest(ImmutableList.of("user-id-1234","user-id-5678"));
+
+    @Test
+    public void testContentType() throws Exception {
+        assertEquals(request.getContentType(), ContentType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void testMethod() throws Exception {
+        assertEquals(request.getHttpMethod(), Request.HttpMethod.POST);
+    }
+
+    @Test
+    public void testHeaders() throws Exception {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(HttpHeaders.CONTENT_TYPE, Request.CONTENT_TYPE_JSON);
+        headers.put(HttpHeaders.ACCEPT, Request.UA_VERSION_JSON);
+
+        assertEquals(request.getRequestHeaders(), headers);
+    }
+
+    @Test
+    public void testURI() throws Exception {
+        URI baseURI = URI.create("https://go.urbanairship.com");
+
+        URI expextedURI = URI.create("https://go.urbanairship.com/api/named_users/uninstall/");
+        assertEquals(request.getUri(baseURI), expextedURI);
+    }
+
+    @Test
+    public void testPushParser() throws Exception {
+        String response = "{\"ok\" : true}";
+        assertEquals(response, request.getResponseParser().parse(response));
+    }
+}
