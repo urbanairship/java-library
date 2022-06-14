a8fd59215af8f9d76448f41c01e524fbe175ac46
Martin V <32457411+martin-vct@users.noreply.github.com>
2022-06-14 11:21:41 +0200

    Update dependencies and update CSVWriter class (#298)

--- src/test/java/com/urbanairship/api/client/UrbanAirshipClientTest.java
+++ src/test/java/com/urbanairship/api/client/UrbanAirshipClientTest.java
@@ -129,7 +129,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 
 public class UrbanAirshipClientTest {
 
