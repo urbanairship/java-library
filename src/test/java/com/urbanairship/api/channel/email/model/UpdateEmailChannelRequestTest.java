b4c1fccd85088f100257cf470dd3baf98aa2f3f5
Martin V <32457411+martin-vct@users.noreply.github.com>
2025-10-06 11:29:39 +0200

    Add Tracking Optin level to UpdateEmailChannel (#358)
    Co-authored-by: martin-vct <martin.vincent@airship.com>

--- src/test/java/com/urbanairship/api/channel/email/model/UpdateEmailChannelRequestTest.java
+++ src/test/java/com/urbanairship/api/channel/email/model/UpdateEmailChannelRequestTest.java
@@ -3,6 +3,7 @@ package com.urbanairship.api.channel.email.model;
 import com.google.common.net.HttpHeaders;
 import com.urbanairship.api.channel.model.email.EmailChannelResponse;
 import com.urbanairship.api.channel.model.email.OptInLevel;
+import com.urbanairship.api.channel.model.email.TrackingOptInLevel;
 import com.urbanairship.api.channel.model.email.UpdateEmailChannel;
 import com.urbanairship.api.channel.model.email.UpdateEmailChannelRequest;
 import com.urbanairship.api.client.Request;
@@ -22,9 +23,12 @@ public class UpdateEmailChannelRequestTest {
     UpdateEmailChannel emailChannel = UpdateEmailChannel.newBuilder()
             .setAddress("example@urbanairship.com")
             .setEmailOptInLevel(OptInLevel.EMAIL_COMMERCIAL_OPTED_IN, "2018-11-11T12:00:00")
+            .setTrackingOptInLevel(TrackingOptInLevel.OPEN_TRACKING_OPTED_IN, "2021-01-01T00:00:00")
+            .setTrackingOptInLevel(TrackingOptInLevel.CLICK_TRACKING_OPTED_OUT, "2021-02-02T00:00:00")
             .build();
 
-    UpdateEmailChannelRequest request = UpdateEmailChannelRequest.newRequest("6c8f1d3a-64d8-4ef9-b7a1-9b128013327e", emailChannel);
+    UpdateEmailChannelRequest request = UpdateEmailChannelRequest.newRequest("6c8f1d3a-64d8-4ef9-b7a1-9b128013327e",
+            emailChannel);
 
     @Test
     public void testContentType() throws Exception {
@@ -38,7 +42,9 @@ public class UpdateEmailChannelRequestTest {
 
     @Test
     public void testBody() throws Exception {
-        assertEquals(request.getRequestBody(), "{\"channel\":{\"type\":\"email\",\"address\":\"example@urbanairship.com\",\"commercial_opted_in\":\"2018-11-11T12:00:00\"}}");
+        assertEquals(
+                request.getRequestBody(),
+                "{\"channel\":{\"type\":\"email\",\"address\":\"example@urbanairship.com\",\"commercial_opted_in\":\"2018-11-11T12:00:00\",\"open_tracking_opted_in\":\"2021-01-01T00:00:00\",\"click_tracking_opted_out\":\"2021-02-02T00:00:00\"}}");
     }
 
     @Test
@@ -54,13 +60,15 @@ public class UpdateEmailChannelRequestTest {
     public void testURI() throws Exception {
         URI baseURI = URI.create("https://go.urbanairship.com");
 
-        URI expectedURI = URI.create("https://go.urbanairship.com/api/channels/email/6c8f1d3a-64d8-4ef9-b7a1-9b128013327e");
+        URI expectedURI = URI
+                .create("https://go.urbanairship.com/api/channels/email/6c8f1d3a-64d8-4ef9-b7a1-9b128013327e");
         assertEquals(request.getUri(baseURI), expectedURI);
     }
 
     @Test
     public void testRegisterEmailChannelParser() throws Exception {
-        ResponseParser<EmailChannelResponse> responseParser = response -> PushObjectMapper.getInstance().readValue(response, EmailChannelResponse.class);
+        ResponseParser<EmailChannelResponse> responseParser = response -> PushObjectMapper.getInstance()
+                .readValue(response, EmailChannelResponse.class);
 
         String response = "{\"ok\" : true,\"channel_id\" : \"6c8f1d3a-64d8-4ef9-b7a1-9b128013327e\"}";
         assertEquals(request.getResponseParser().parse(response), responseParser.parse(response));
